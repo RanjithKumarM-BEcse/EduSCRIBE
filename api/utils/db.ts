bfe098a310961a08be480e80a341b67e2c37fa80
Arvind <arvind152402@gmail.com>
2026-09-23 00:00:56 +0530

    fix: map custom MY_AWS environment variables to DynamoDB credentials

--- api/utils/db.ts
+++ api/utils/db.ts
@@ -2,7 +2,11 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({
-  region: 'eu-north-1' // Hardcoded because Vercel automatically overrides AWS_REGION with its own lambda region
+  region: 'eu-north-1', // Hardcoded because Vercel automatically overrides AWS_REGION with its own lambda region
+  credentials: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY as string
+  }
 });
 
 export const docClient = DynamoDBDocumentClient.from(client);
